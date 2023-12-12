a39ea9f0f5f6b4ab617a34edec31c9dc626a4bb2
JuanMartinezXMBs <142538243+JuanMartinezXMBs@users.noreply.github.com>
2023-12-12 17:12:10 -0300

    att

--- vinculados/rodape.php
+++ vinculados/rodape.php
@@ -1,7 +1,16 @@
 <footer>
-    <h3>Plena Paz - JD. Eliane - SP</h3>
-
-    <p>CNPJ: 66.416.103/0001-45 (F)</p>
-</footer>
+        <br>
+<p>Simplicity é um site fictício desenvolvido para fins educacionais por alunos do Curso Programador Web - 2023 &copy; <b>Senac Penha</b> - <time>2023</time>   </p>
+    </footer>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="lightbox/js/lightbox.min.js"></script> 
+    
+    <script>
+    lightbox.option({
+        albumLabel: "Foto %1 de %2"
+    })
+    </script>
+    
+    <script src="js/menu.js"></script>
 </body>
 </html>
