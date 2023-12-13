4bede9df88272513dfee1d0164b210aa7abf6a9c
JuanMartinezXMBs <142538243+JuanMartinezXMBs@users.noreply.github.com>
2023-12-13 15:17:45 -0300

    att

--- vinculados/rodape.php
+++ vinculados/rodape.php
@@ -1,9 +1,23 @@
 <footer>
+    <div class="menurodape">
     <h3>Plena Paz - JD. Eliane - SP</h3>
 
     <p>CNPJ: 66.416.103/0001-45 (F)</p>
 
     <p>desenvolvido para fins educacionais por alunos do Curso Programador Web - 2023</p>
+
+    
+            <nav>
+                <ul>
+                <li><a href="quem-somos.php">Quem somos</a></li>
+                <li><a href="projetos.php">Projetos</a></li>
+                <li><a href="galeria.php">Galeria</a></li>
+                <li><a href="noticias.php">Noticias</a></li>
+                <li><a href="Contatos.php">Contatos</a></li>
+                <li><a href="login.php">Login</a></li>
+                </ul>
+            </nav>
+            </div>
 </footer>
 </body>
 
