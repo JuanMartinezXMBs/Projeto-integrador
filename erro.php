1da701ef20f3da0b0fabee142c6bdb1c26c379a7
marcelogomesandrade <gomesgomes123321@gmail.com>
2023-12-13 16:33:30 -0300

    att

---
+++ erro.php
@@ -0,0 +1,29 @@
+<?php
+require "vinculados/cabecalho.php";
+?>
+
+    <main>
+    <div class="centralizar">
+
+    <div class="box">
+        <figure>
+            <img src="imagens/cristianismo.jpg" alt="">
+        </figure>
+    </div>
+
+    <div class="centralizar box horarios texto">
+        <h2>
+            EM BREVE ESTAREMOS COLOCANDO O NOSSO PROJETO NO AR 
+        </h2>
+        
+    </div>
+    
+    
+    <div class="box1 "></div>
+
+    </div>
+</main>
+
+<?php 
+require_once "vinculados/rodape.php";
+?>
--- projetos.php
+++ projetos.php
@@ -20,11 +20,31 @@ require "vinculados/cabecalho.php";
     
     <div class="box2">
             <figure>
-                <img src="imagens/imagem_05_jesus.jpg" alt="">
+            <a href="erro.php"><img src="imagens/centro_cuidado.jpg" alt=""></a>  
             </figure>
-            <h2 class="texto">Centro de Cuidado Pastoral </h2>
+            <h2 class="texto"><a href="erro.php">Centro de Cuidado Pastoral </a></h2>
         </div>
-    
+
+        <div class="box2">
+            <figure>
+            <a href="erro.php"><img src="imagens/cuidando_kids.jpg" alt=""></a>
+            </figure>
+            <h2 class="texto"><a href="erro.php">Educando as Nossas Crianças (Quiz) </a></h2>
+</div>
+<div class="box2">
+            <figure>
+            <a href="erro.php"><img src="imagens/projeto_resgate.jpg" alt=""></a> 
+            </figure>
+            <h2 class="texto"><a href="erro.php">Projeto Resgate </a></h2>
+        </div>
+        <div class="box2">
+            <figure>
+            <a href="erro.php"><img src="imagens/caminhando_estradas.jpg" alt=""></a>
+            </figure>
+            <h2 class="texto"> <a href="erro.php">Caminhando pelas Estradas </a> </h2>
+        </div>
+
+
     
 
     </div>
