cf847073605134af5bca7cf02d80ff733e861069
JuanMartinezXMBs <142538243+JuanMartinezXMBs@users.noreply.github.com>
2023-12-13 15:09:42 -0300

    att

--- index.php
+++ index.php
@@ -1,38 +1,47 @@
 <?php
 require "vinculados/cabecalho.php";
 ?>
-    <main>
+<main>
     <div class="centralizar ">
 
-    <div class="box">
-        <figure>
-            <img src="imagens/imagem_05_jesus.jpg" alt="">
-        </figure>
+        <div class="box">
+            <figure>
+                <img src="imagens/imagem_05_jesus.jpg" alt="">
+            </figure>
+        </div>
+
+        <div class="centralizar box horarios">
+            <h2>
+                Campus - Jd. Eliane SP
+            </h2>
+            <br>
+            <p>
+                Sábado: 17h | Domingo: 9h30 - 11h30 - 16h30 -
+                18h30 e 20h30 | Segunda: 20h30
+            </p>
+        </div>
+
+        <div class="box teste container1">
+
+            <div class="color">
+            <div>
+                <h2>Lorem ipsum dolor sit amet consectetur</h2>
+            </div>
+
+            <div >
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident numquam blanditiis nesciunt aspernatur et culpa, dolorum laboriosam recusandae quo?</p>
+            </div>
+            </div>
+
+        </div>
+
     </div>
 
-    <div class="centralizar box horarios">
-        <h2>
-            Campus - Jd. Eliane  SP
-        </h2>
-        <br>
-        <p>
-            Sábado: 17h | Domingo: 9h30 - 11h30 - 16h30 -
-            18h30 e 20h30 | Segunda: 20h30
-        </p>
-    </div>
-    
-    <div class="box container1">
-    <h2>Lorem ipsum dolor sit amet consectetur</h2>
-    
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident numquam blanditiis nesciunt aspernatur et culpa, dolorum laboriosam recusandae quo?</p>
-    </div>
-
-    </div>
-    <div class="box1 "></div>
+    <div class="box1 "></div> <!-- espaço vazio -->
 
     </div>
 </main>
 
-<?php 
+<?php
 require_once "vinculados/rodape.php";
 ?>
