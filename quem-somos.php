59eb6097cc6fc6f6f040cba43dbc3d460c92bc79
marcelogomesandrade <gomesgomes123321@gmail.com>
2023-12-12 23:23:26 -0300

    att

--- quem-somos.php
+++ quem-somos.php
@@ -3,7 +3,7 @@ require "vinculados/cabecalho.php";
 ?>
 
     <main>
-    <div class="centralizar ">
+    <div class="centralizar">
 
     <div class="box">
         <figure>
@@ -15,28 +15,28 @@ require "vinculados/cabecalho.php";
         <h2>
             QUEM SOMOS 
         </h2>
-        <p>
+        <p class="centralizar">
         A CHURCH é uma igreja muito receptiva e cuidadosa com todos os seus membros e não membros, somos uma grande família! </p>
 
 
-        <p>Nossos cultos são sempre bem alegres, e como muitos louvores e adoração a Deus. </p>
+        <p class="centralizar" >Nossos cultos são sempre bem alegres, e como muitos louvores e adoração a Deus. </p>
 
-        <p>Nossas ministrações sempre com o intuito de levar a verdadeira mensagem de Deus para todos os ouvintes.</p> 
+        <p class="centralizar" >Nossas ministrações sempre com o intuito de levar a verdadeira mensagem de Deus para todos os ouvintes.</p> 
 
-        <p>Com certeza ficara marcado em sua vida. </p>
+        <p class="centralizar">Com certeza ficara marcado em sua vida. </p>
 
-        <p>A CHURCH é o fruto de um sonho de Deus para o coração do Pastor Presidente Flavio Checca e a sua esposa Pastora e Missionaria  
+        <p class="centralizar">A CHURCH é o fruto de um sonho de Deus para o coração do Pastor Presidente Flavio Checca e a sua esposa Pastora e Missionaria  
 Tania Maria da S. S. Checca. </p>
 
-<p>Você e sua família são os nossos convidados especiais. </p>
+<p class="centralizar" >Você e sua família são os nossos convidados especiais. </p>
 
 <h3>Missão </h3>
 
-<p>Ganhar mais vidas para o reino de Deus e que sejam transformados e impactados para que também possam compartilhar dessa graça transformadora. </p>
+<p class="centralizar">Ganhar mais vidas para o reino de Deus e que sejam transformados e impactados para que também possam compartilhar dessa graça transformadora. </p>
 
 <h3>Visão </h3>
 
-<p>A visão da nossa igreja está pautada em sentimentos que Deus foi colocando ao longo do tempo no coração de sua liderança. Queremos ser uma igreja que cresce em direção a Deus, num compromisso sério com Ele, buscando conhecê-Lo na Sua intimidade, poder, Graça e em Sua força.</p> 
+<p class="centralizar">A visão da nossa igreja está pautada em sentimentos que Deus foi colocando ao longo do tempo no coração de sua liderança. Queremos ser uma igreja que cresce em direção a Deus, num compromisso sério com Ele, buscando conhecê-Lo na Sua intimidade, poder, Graça e em Sua força.</p> 
 
 <h3>Proposito </h3>
 
