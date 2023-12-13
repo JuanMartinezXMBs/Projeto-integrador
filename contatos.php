242ae4c22a1a0407f9e4bd2fde32791446e2e3a3
JuanMartinezXMBs <142538243+JuanMartinezXMBs@users.noreply.github.com>
2023-12-13 15:30:35 -0300

    att

--- contatos.php
+++ contatos.php
@@ -20,29 +20,11 @@ require "vinculados/cabecalho.php";
         </p>
     </div>
     
-    <div class="box container1">
-    <h2>Lorem ipsum dolor sit amet consectetur</h2>
-    
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident numquam blanditiis nesciunt aspernatur et culpa, dolorum laboriosam recusandae quo?</p>
-    </div>
+    <div class="box mapa">
+            
+    <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3657.2017933249026!2d-46.5924968246699!3d-23.56119497879991!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sRua%20Pascoal%20Mangueira%2C%20200%20-%20Vila%20Mariana-%20S%C3%A3o%20Paulo%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1702492078290!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
-    <div class="box container1">
-    <h2>Lorem ipsum dolor sit amet consectetur</h2>
-    
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident numquam blanditiis nesciunt aspernatur et culpa, dolorum laboriosam recusandae quo?</p>
-    </div>
-
-    <div class="box container1">
-    <h2>Lorem ipsum dolor sit amet consectetur</h2>
-    
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident numquam blanditiis nesciunt aspernatur et culpa, dolorum laboriosam recusandae quo?</p>
-    </div>
-
-    <div class="box container1">
-    <h2>Lorem ipsum dolor sit amet consectetur</h2>
-    
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident numquam blanditiis nesciunt aspernatur et culpa, dolorum laboriosam recusandae quo?</p>
-    </div>
+        </div>
 
     </div>
     <div class="box1 "></div>
