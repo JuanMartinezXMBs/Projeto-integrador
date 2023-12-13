15f7135468088a62338ebd861c4a92e0cf9aa2bd
Paola82254 <teacherpaola4kids@gmail.com>
2023-12-13 14:35:24 -0300

    galeria-update

--- galeria.php
+++ galeria.php
@@ -1,58 +1,122 @@
+<link rel="stylesheet" href="estilos.css">
 <?php
 require "vinculados/cabecalho.php";
 ?>
-
-    <main>
-    <div class="centralizar ">
-
-    <div class="box">
-        <figure>
-            <img src="imagens/respingo-colorido-abstrato-3d-background-generativo-ai-background_60438-2509.avif" alt="">
-        </figure>
-    </div>
-
-    <div class="centralizar box horarios">
-        <h2>
-            CAMPUS ANÁLIA FRANCO 
-        </h2>
-        <p>
-            Sábado: 17h | Domingo: 9h30 - 11h30 - 16h30
-            18h30 e 20h30 | Segunda: 20h30
-        </p>
-    </div>
-    
-    <div class="box container1">
-    <h2>A comunidade</h2>
-    
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident numquam blanditiis nesciunt aspernatur et culpa, dolorum laboriosam recusandae quo?</p>
-    </div>
-
-    <div class="box container1">
-    <h2>Culto Kids</h2>
-    <div>
-    <img src="./imagens-editadas/foto-38-editada.jpg" alt="">
-    
-
-    <p>Nossa Igreja conta com uma área para as crianças destinada para seu aprendizado das Escrituras Sagradas de uma forma lúdica e interativa. Os cultos Kids ocorrem todos os Sábados</p>
-    </div>
-
-    <div class="box container1">
-    <h2>Lorem ipsum dolor sit amet consectetur</h2>
-    
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident numquam blanditiis nesciunt aspernatur et culpa, dolorum laboriosam recusandae quo?</p>
-    </div>
-
-    <div class="box container1">
-    <h2>Lorem ipsum dolor sit amet consectetur</h2>
-    
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident numquam blanditiis nesciunt aspernatur et culpa, dolorum laboriosam recusandae quo?</p>
-    </div>
-
-    </div>
-    <div class="box1 "></div>
-
-    </div>
-</main>
+<h1><p class="titulodagaleria">GALERIA DE FOTOS</p></h1>
+<div>
+    <figure></figure>
+</div>
+<div>
+    <figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
+<div>
+<figure></figure>
+</div>
     
 <?php 
 require_once "vinculados/rodape.php";
