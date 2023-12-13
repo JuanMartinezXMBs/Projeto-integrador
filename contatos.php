9a044291e892fdb266a0e417b6f12c61a103e912
marcelogomesandrade <gomesgomes123321@gmail.com>
2023-12-13 15:31:21 -0300

    ppp

--- contatos.php
+++ contatos.php
@@ -20,32 +20,13 @@ require "vinculados/cabecalho.php";
         </p>
     </div>
     
-    <div class="box container1">
-    <h2>Lorem ipsum dolor sit amet consectetur</h2>
     
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident numquam blanditiis nesciunt aspernatur et culpa, dolorum laboriosam recusandae quo?</p>
+    <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.175765503498!2d-46.54283012381813!3d-23.526179860302186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5e45de28bb23%3A0x1d2ed3122aae28a7!2sSenac%20Penha!5e0!3m2!1spt-BR!2sbr!4v1695150894837!5m2!1spt-BR!2sbr"
+                width="800" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
-
-    <div class="box container1">
-    <h2>Lorem ipsum dolor sit amet consectetur</h2>
     
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident numquam blanditiis nesciunt aspernatur et culpa, dolorum laboriosam recusandae quo?</p>
-    </div>
-
-    <div class="box container1">
-    <h2>Lorem ipsum dolor sit amet consectetur</h2>
-    
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident numquam blanditiis nesciunt aspernatur et culpa, dolorum laboriosam recusandae quo?</p>
-    </div>
-
-    <div class="box container1">
-    <h2>Lorem ipsum dolor sit amet consectetur</h2>
-    
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident numquam blanditiis nesciunt aspernatur et culpa, dolorum laboriosam recusandae quo?</p>
-    </div>
-
-    </div>
-    <div class="box1 "></div>
 
     </div>
 </main>
