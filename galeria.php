35afa46e6ac2972f7e0a64b530bd1204b5dddeee
Paola82254 <teacherpaola4kids@gmail.com>
2023-12-14 16:38:29 -0300

    galeria-finalizada

--- galeria.php
+++ galeria.php
@@ -6,7 +6,7 @@ require "vinculados/cabecalho.php";
     <p>GALERIA DE FOTOS</p>
 </h1>
 
-<div class="warpcentralizar">
+<div class="wrapcentralizar">
 
 <div class="foto">
     <figure class="primeira_foto">
@@ -15,136 +15,134 @@ require "vinculados/cabecalho.php";
 </div>
 <div class="foto">
     <figure  class="segunda_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_3_editada.jpg"><img src="./imagens-editadas-mini/foto_3_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_3_editada.jpg"><img src="./imagens-editadas-mini/foto_3_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure  class="terceira_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_4_editada.jpg"><img src="./imagens-editadas-mini/foto_4_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_4_editada.jpg"><img src="./imagens-editadas-mini/foto_4_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure  class="quarta_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_5_editada.jpg"><img src="./imagens-editadas-mini/foto_5_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_5_editada.jpg"><img src="./imagens-editadas-mini/foto_5_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure class="quinta_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_6_editada.jpg"><img src="./imagens-editadas-mini/foto_6_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_6_editada.jpg"><img src="./imagens-editadas-mini/foto_6_editada_mini.png" alt=""></a>
 
     </figure>
 </div>
 <div class="foto">
     <figure class="sexta_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_7_editada.jpg">
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_7_editada.jpg">
         <img src="./imagens-editadas-mini/foto_7_editada_mini.png" alt="">
     </a>
     </figure>
 </div>
 <div class="foto">
     <figure class="setima_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_8_editada.jpg"><img src="./imagens-editadas-mini/foto_8_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_8_editada.jpg"><img src="./imagens-editadas-mini/foto_8_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure class="oitava_foto">
-        <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_9_editada.jpg"><img src="./imagens-editadas-mini/foto_9_editada_mini.png" alt="">
+        <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_9_editada.jpg"><img src="./imagens-editadas-mini/foto_9_editada_mini.png" alt="">
         </a>
     </figure>
 </div>
 <div class="foto">
     <figure class="nona_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos"  href="./imagens-editadas/foto_10_editada.jpg"><img src="./imagens-editadas-mini/foto_10_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos"  href="./imagens-editadas/foto_10_editada.jpg"><img src="./imagens-editadas-mini/foto_10_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure class="decima_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_11_editada.jpg"><img src="./imagens-editadas-mini/foto_11_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_11_editada.jpg"><img src="./imagens-editadas-mini/foto_11_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure class="decimaprimeira_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_12_editada.jpg"><img src="./imagens-editadas-mini/foto_12_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_12_editada.jpg"><img src="./imagens-editadas-mini/foto_12_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure class="decimasegunda_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_13_editada.jpg"><img src="./imagens-editadas-mini/foto_13_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_13_editada.jpg"><img src="./imagens-editadas-mini/foto_13_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure class="decimaterceira_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_14_editada.jpg"><img src="./imagens-editadas-mini/foto_14_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_14_editada.jpg"><img src="./imagens-editadas-mini/foto_14_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure class="decimaquarta_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_15_editada.jpg"><img src="./imagens-editadas-mini/foto_15_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_15_editada.jpg"><img src="./imagens-editadas-mini/foto_15_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure class="decimasexta_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_23_editada.jpg"><img src="./imagens-editadas-mini/foto_23_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_23_editada.jpg"><img src="./imagens-editadas-mini/foto_23_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure class="decimasetima_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_24_editada.jpg"><img src="./imagens-editadas-mini/foto_24_editada_mini.png" alt=""></a>
-    </figure>
-</div>
-<div class="foto">
-    <figure class="decimaoitava_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_25_editada.jpg"><img src="./imagens-editadas-mini/foto_25_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_24_editada.jpg"><img src="./imagens-editadas-mini/foto_24_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure class="decimanona_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_26_editada.jpg"><img src="./imagens-editadas-mini/foto_26_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_26_editada.jpg"><img src="./imagens-editadas-mini/foto_26_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure class="vigesimasegunda_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_29_editada.jpg"><img src="./imagens-editadas-mini/foto_29_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_29_editada.jpg"><img src="./imagens-editadas-mini/foto_29_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure class="vigesimaterceira_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_30_editada.jpg"><img src="./imagens-editadas-mini/foto_30_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_30_editada.jpg"><img src="./imagens-editadas-mini/foto_30_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure class="vigesimaquinta_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_32_editada.jpg"><img src="./imagens-editadas-mini/foto_32_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_32_editada.jpg"><img src="./imagens-editadas-mini/foto_32_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure class="vigesimasexta_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_33_editada.jpg"><img src="./imagens-editadas-mini/foto_33_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_33_editada.jpg"><img src="./imagens-editadas-mini/foto_33_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure class="vigesimasetima_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_34_editada.jpg"><img src="./imagens-editadas-mini/foto_34_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_34_editada.jpg"><img src="./imagens-editadas-mini/foto_34_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure class="vigesimaoitava_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_35_editada.jpg"><img src="./imagens-editadas-mini/foto_35_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_35_editada.jpg"><img src="./imagens-editadas-mini/foto_35_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure class="vigesimanona_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_36_editada.jpg"><img src="./imagens-editadas-mini/foto_36_editada_mini.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_36_editada.jpg"><img src="./imagens-editadas-mini/foto_36_editada_mini.png" alt=""></a>
     </figure>
 </div>
 <div class="foto">
     <figure class="trigesimaprimeira_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto-38-editada.jpg"><img src="./imagens-editadas-mini/foto_38_editada_mini.png alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto-38-editada.jpg"><img src="./imagens-editadas-mini/foto_38_editada_mini.png" alt=""></a>
     </figure>
+</div>
 <div class="foto">
     <figure class="decimaquinta_foto">
-    <a data-title="Desenvolvimento de sites" data-lightbox="fotos" href="./imagens-editadas/foto_1_editada.jpg"><img src="./imagens-editadas-mini/foto_1_editada_mini.jpg.png" alt=""></a>
+    <a data-title="fotos da igreja" data-lightbox="fotos" href="./imagens-editadas/foto_1_editada.jpg"><img src="./imagens-editadas-mini/foto_1_editada_mini.jpg" alt=""></a>
     </figure>
+</div>
+
 </div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
