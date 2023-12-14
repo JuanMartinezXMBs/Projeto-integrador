b9fd0f0b882230ca8b580b9a6a8763993b90c82a
Paola82254 <teacherpaola4kids@gmail.com>
2023-12-14 17:28:33 -0300

    noticias-atualizadas-fim

--- noticias.php
+++ noticias.php
@@ -3,6 +3,7 @@ require "vinculados/cabecalho.php";
 ?>
 
 <main>
+
     <div class="centralizar ">
 
         <div class="box">
@@ -17,67 +18,66 @@ require "vinculados/cabecalho.php";
             </h2>
 
         </div>
-
-        <div class="box teste container1">
-
-            <div class="color">
-                <div>
-                    <h2>Nossa missão em São Paulo</h2>
-                </div>
-
-                <div>
-                    <p>Deus tem uma missão para seu povo que mora nas grandes cidades. Deus tem missão urbana para a Igreja que fica na cidade de São Paulo.</p>
-                </div>
-            </div>
-
-        </div>
         <div class="todas-as-noticias">
-        <div class="box teste container2">
+            <div class="box teste container1">
 
-            <div class="color">
-                <div>
-                    <h2>Há Esperança para aquele que pecou</h2>
+                <div class="color">
+                    <div>
+                        <h2>Nossa missão em São Paulo</h2>
+                    </div>
+
+                    <div>
+                        <p>Deus tem uma missão para seu povo que mora nas grandes cidades. Deus tem missão urbana para a Igreja que fica na cidade de São Paulo.</p>
+                    </div>
                 </div>
 
-                <div>
-                    <p>“Aquele que encobre as suas transgressões jamais prosperará, mas quem as confessa e deixa alcançará misericórdia” (Provérbios 28.13). </p>
-                </div>
             </div>
 
-        </div>
 
-        <div class="box teste container3">
+            <div class="box teste container2">
 
-            <div class="color">
-                <div>
-                    <h2>O Conhecimento de Deus, uma Verdade Consoladora</h2>
+                <div class="color">
+                    <div>
+                        <h2>Há Esperança para aquele que pecou</h2>
+                    </div>
+
+                    <div>
+                        <p>“Aquele que encobre as suas transgressões jamais prosperará, mas quem as confessa e deixa alcançará misericórdia” (Provérbios 28.13). </p>
+                    </div>
                 </div>
 
-                <div>
-                    <p>“O Senhor é bom, é fortaleza no dia da angústia e conhece os que nele se refugiam” (Na 1.7). Vamos analisar essas três verdades.</p>
-                </div>
             </div>
 
-        </div>
+            <div class="box teste container3">
 
-        <div class="box teste container4">
+                <div class="color">
+                    <div>
+                        <h2>O Conhecimento de Deus, uma Verdade Consoladora</h2>
+                    </div>
 
-            <div class="color">
-                <div>
-                    <h2>Os Pilares da Reforma Protestante</h2>
+                    <div>
+                        <p>“O Senhor é bom, é fortaleza no dia da angústia e conhece os que nele se refugiam” (Na 1.7). Vamos analisar essas três verdades.</p>
+                    </div>
                 </div>
 
-                <div>
-                    <p>A Reforma Protestante foi o maior acontecimento na igreja cristã depois do Pentecostes.</p>
-                </div>
             </div>
 
-        </div>
+            <div class="box teste container4">
+
+                <div class="color">
+                    <div>
+                        <h2>Os Pilares da Reforma Protestante</h2>
+                    </div>
+
+                    <div>
+                        <p>A Reforma Protestante foi o maior acontecimento na igreja cristã depois do Pentecostes.</p>
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
-    <div class="box1 "></div>
 
-    </div>
 </main>
 
 <?php
