e67629e754509702bb164ffdeb44463ae56289be
Paola82254 <teacherpaola4kids@gmail.com>
2023-12-14 16:55:04 -0300

    update-noticias

--- noticias.php
+++ noticias.php
@@ -2,60 +2,74 @@
 require "vinculados/cabecalho.php";
 ?>
 
-    <main>
+<main>
     <div class="centralizar ">
 
-    <div class="box">
-        <figure>
-            <img src="imagens/imagem_04_cruz.jpg" alt="">
-        </figure>
-    </div>
+        <div class="box">
+            <figure>
+                <img src="imagens/imagem_04_cruz.jpg" alt="">
+            </figure>
+        </div>
 
-    <div class="centralizar box horarios">
-        <h2>
-           NOTÍCIAS | BLOG  
-        </h2>
-        
-    </div>
-    
-    <div class="box teste container1">
+        <div class="centralizar box horarios">
+            <h2>
+                NOTÍCIAS | BLOG
+            </h2>
+
+        </div>
+
+        <div class="box teste container1">
 
             <div class="color">
-            <div>
-                <h2>Nossa missão em São Paulo</h2>
-            </div>
+                <div>
+                    <h2>Nossa missão em São Paulo</h2>
+                </div>
 
-            <div >
-                <p>Deus tem uma missão para seu povo que mora nas grandes cidades. Deus tem missão urbana para a Igreja que fica na cidade de São Paulo.</p>
-            </div>
+                <div>
+                    <p>Deus tem uma missão para seu povo que mora nas grandes cidades. Deus tem missão urbana para a Igreja que fica na cidade de São Paulo.</p>
+                </div>
             </div>
 
         </div>
 
-    <div class="box teste container1">
+        <div class="box teste container1">
 
             <div class="color">
-            <div>
-                <h2>Lorem ipsum dolor sit amet consectetur</h2>
-            </div>
+                <div>
+                    <h2>Há Esperança para aquele que pecou</h2>
+                </div>
 
-            <div >
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident numquam blanditiis nesciunt aspernatur et culpa, dolorum laboriosam recusandae quo?</p>
-            </div>
+                <div>
+                    <p>“Aquele que encobre as suas transgressões jamais prosperará, mas quem as confessa e deixa alcançará misericórdia” (Provérbios 28.13). </p>
+                </div>
             </div>
 
         </div>
 
-    <div class="box teste container1">
+        <div class="box teste container1">
 
             <div class="color">
-            <div>
-                <h2>Lorem ipsum dolor sit amet consectetur</h2>
+                <div>
+                    <h2>Lorem ipsum dolor sit amet consectetur</h2>
+                </div>
+
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident numquam blanditiis nesciunt aspernatur et culpa, dolorum laboriosam recusandae quo?</p>
+                </div>
             </div>
 
-            <div >
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident numquam blanditiis nesciunt aspernatur et culpa, dolorum laboriosam recusandae quo?</p>
-            </div>
+        </div>
+
+        <div class="box teste container1">
+
+            <div class="color">
+                <div>
+                    <h2>Lorem ipsum dolor sit amet consectetur</h2>
+                </div>
+
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio provident numquam blanditiis nesciunt aspernatur et culpa, dolorum laboriosam recusandae quo?</p>
+                </div>
             </div>
 
         </div>
@@ -66,6 +80,6 @@ require "vinculados/cabecalho.php";
     </div>
 </main>
 
-<?php 
+<?php
 require_once "vinculados/rodape.php";
 ?>
